36b9aceaddff80a3eabc91c913127d7cb28d834d
asotog <asotog88@yahoo.es>
2014-03-05 10:45:55 -0600

    Control language highlighting from json file called languages.json

--- main.js
+++ main.js
@@ -9,22 +9,18 @@ var define, brackets
         var CommandManager = brackets.getModule("command/CommandManager");
         var Menus = brackets.getModule("command/Menus");
 
+        var updateCurrentHighlighterLabel = function(label) {
+            $("#code-highlighter-switcher .label").text(label);
+        };
+
         /* Status bar item creation */
-        var statusComponentHtml = "<div id=\"code-highlighter-switcher\">Code Highlighter Switcher</div>";
+        var statusComponentHtml = "<div id=\"code-highlighter-switcher\">Code Highlighter Switcher: <strong class=\"label\">Default</strong></div>";
         StatusBar.addIndicator("code-highlighter-switcher", $(statusComponentHtml), true, "", "", "status-indent");
 
         /* Languages */
-        var languages = [{
-            label: 'HTML',
-            mode: 'htmlmixed'
-        }, {
-            label: 'PHP',
-            mode: 'php'
-        }, {
-            label: 'Javascript',
-            mode: 'javascript'
-        }];
-
+        var languages = require("text!languages.json");
+        languages = JSON.parse(languages);
+        
         var COMMAND_ = "highlightercommand."
         var showLanguagesOptionsMenu = Menus.registerContextMenu("code-highlighter-switcher-menu");
 
@@ -34,11 +30,12 @@ var define, brackets
                 CommandManager.register(label, command, function () {
                     //this.setChecked(true)
                     EditorManager.getCurrentFullEditor()._codeMirror.setOption("mode", mode);
+                    updateCurrentHighlighterLabel(label);
                 });
                 showLanguagesOptionsMenu.addMenuItem(command);
             })(languages[i].label, COMMAND_ + languages[i].mode, languages[i].mode);
         }
-
+            
         /* Show context menu */
         $("#code-highlighter-switcher").on("contextmenu", function (e) {
             showLanguagesOptionsMenu.open(e);
